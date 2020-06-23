ad5f4ccef7bdb0cad1c891e98062610121be5964
Ondřej Ezr <oezr@redhat.com>
2020-06-23 03:31:38 +0200

    Add basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 ForemanPuppetEnc::Engine.routes.draw do
-  get 'new_action', to: 'foreman_puppet_enc/hosts#new_action'
   get 'foreman_puppet_enc', to: 'foreman_puppet_enc/react#index'
   resources :config_groups, except: [:show] do
     collection do
@@ -8,9 +7,24 @@ ForemanPuppetEnc::Engine.routes.draw do
     end
   end
 
+  get 'puppetclass_lookup_keys/help', :action => :welcome, :controller => 'puppetclass_lookup_keys'
+
+  constraints(:id => /[^\/]+/) do
+    resources :puppetclass_lookup_keys, :except => [:show, :new, :create] do
+      resources :lookup_values, :only => [:index, :create, :update, :destroy]
+      collection do
+        get 'auto_complete_search'
+      end
+    end
+  end
+
   namespace :api, defaults: { format: 'json' } do
     scope '(:apiv)', module: :v2, defaults: { apiv: 'v2' }, apiv: /v1|v2/, constraints: ApiConstraints.new(version: 2, default: true) do
-      get 'new_action', to: 'foreman_puppet_enc/api/v2/hosts#new_action'
+      constraints(:id => /[^\/]+/) do
+        resources :smart_class_parameters, :except => [:new, :edit, :create, :destroy] do
+          resources :override_values, :except => [:new, :edit]
+        end
+      end
     end
   end
 end
